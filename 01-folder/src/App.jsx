f6844c1c111f84de8474935ecfed0def25ead223
shavneetkaur1234-spec <shavneetkaur1234@gmail.com>
2026-03-14 17:14:29 +0530

    01-folder done

---
+++ 01-folder/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>                                                
+    <div id='dad'>
+      <h1 id='child1'></h1>
+      <h2 id='child2'></h2>
+    </div>
+    <div id='mom'></div>
+    </>
+  )
+}
+
+export default App
